c49b5fb84bec5fd2624d170f20cd0f164d09949a
Erin Otterstrom <hapiotter@gmail.com>
2020-04-25 20:03:56 -0600

    finished weather dashboard

--- script.js
+++ script.js
@@ -1,100 +1,163 @@
+ var APIKey1 = "397e528f419d411919453c9941235a39";
+ var APIKey2 = "25eae694827e9cccc433d37950486b7c";
+
+ var cityList = JSON.parse(window.localStorage.getItem("cityList")) || [];
 $(document).ready(function(){
-  //Creating the table
-  // tr = $('<tr>');
-  // searchCol = $('<td>');
-  // weatherCol = $('<td>');
-  // searchBox = $(`<textarea id=citySearch class = "input" rows="2" cols="20">`)
   
-    // function currentDate() {
       let date = moment().format("dddd, MMMM Do YYYY");
       $('#currentDay').append(date); 
-    //  }
-    //currentDate();
 
-    // First API key variable
-    var APIKey1 = "397e528f419d411919453c9941235a39";
-    // Building the URL we need to query the weather database
-    var queryURL = "https://api.openweathermap.org/data/2.5/weather?" +
-      "q=Denver&appid=" + APIKey1 + "&units=imperial";
+      loadHistoryList(cityList);
+      getCurrentWeather(cityList[cityList.length - 1]);
 
-    //AJAX call to the OpenWeatherMap API
-    $.ajax({
-      url: queryURL,
-      method: "GET"
-    })
-      // Store the retrieved data inside an object called "response"
-      .then(function(response) {
-        tr = $('#tr');
-        city = $('<td>');
-        // Log the queryURL
-        console.log(queryURL);
+      $("#history").on("click", "li", function() {
+            getCurrentWeather($(this).text());
+          });
+            
+// This function handles events where the Search button is clicked
+$("#searchBtn").on("click", function(event) {
+  event.preventDefault();
+  // This line grabs the input from the textbox
+  var cityInput = $("#cityInput").val();
+  $("#cityInput").val('');
+  getCurrentWeather(cityInput);
+  addCityToLocaltorage(cityInput);
 
-        // Log the resulting object
-        console.log(response);
-
-        // Transfer content to HTML
-        $("<.city>").text("City: " + response.name);
-        $("<.date>").text("Date: " + date);
-        $("<.temp>").text("Temperature: " + response.main.temp);
-        $("<.humidity>").text("Humidity: " + response.main.humidity);
-        $("<.windSpeed>").text("Wind Speed: " + response.wind.speed);
-        $("<.icon>").text("Icon: " + response.weather.icon);
-        tr.append("City: " + response.name,
-          "<br>", "Date: " + date, 
-          "<br>", "Temperature: " + response.main.temp, 
-          "<br>", "Humidity: " + response.main.humidity,
-          "<br>", "Wind Speed: " + response.wind.speed);
-        // $(".col-sm-9").append(tr);
-        // $("#dashboard").append(".col-sm-9");
-
-        // Log the data in the console
-        console.log("City: " + response.name);
-        console.log("Date: " + response.date);
-        console.log("Humidity: " + response.main.humidity);
-        console.log("Temperature: " + response.main.temp);
-        console.log("Icon: " + response.weather.icon)
-      });
-
-    // Second API Key
-    var APIKey2 = "25eae694827e9cccc433d37950486b7c";
-    // Building the URL we need to query the forecast database
-    var queryURL = "https://api.openweathermap.org/data/2.5/forecast?" +
-      "q=Denver&appid=" + APIKey2 + "&units=imperial";
-
-    // AJAX call to the OpenWeatherMap API
-    $.ajax({
-      url: queryURL,
-      method: "GET"
-    })
-      // Store the retrieved data inside an object called "response"
-      .then(function(response) {
-
-        // Log the queryURL
-        console.log(queryURL);
-
-        // Log the resulting object
-        console.log(response);
-
-        // Transfer content to HTML
-        $("<.city>").text("City: " + response.city.name);
-        $("<.date>").text("Date: " + response.list[4].dt_txt);
-        $("<.humidity1>").text("Humidity1: " + response.list[4].main.humidity);
-        $("<.humidity2>").text("Humidity2: " + response.list[12].main.humidity);
-        $("<.humidity3>").text("Humidity3: " + response.list[20].main.humidity);
-        $("<.humidity4>").text("Humidity4: " + response.list[28].main.humidity);
-        $("<.humidity5>").text("Humidity5: " + response.list[36].main.humidity);
-        $("<.temp>").text("Temperature: " + response.list[4].main.temp);
-        $("<.icon>").text("Icon: " + response.list[4].weather[0].icon);
-        
-        // Log the data in the console
-        console.log("City: " + response.city.name);
-        console.log("Date: " + response.list[4].dt_txt);
-        console.log("Humidity1: " + response.list[4].main.humidity);
-        console.log("Humidity2: " + response.list[12].main.humidity);
-        console.log("Humidity3: " + response.list[20].main.humidity);
-        console.log("Humidity4: " + response.list[28].main.humidity);
-        console.log("Humidity5: " + response.list[36].main.humidity);
-        console.log("Temperature: " + response.list[4].main.temp);
-        console.log("Icon: " + response.list[4].weather[0].icon);
-      });
 });
+
+});
+
+function loadHistoryList(){
+      for (var i = 0; i < cityList.length; i++) {
+            createHistoryLi(cityList[i]);
+          }
+}
+
+function addCityToLocaltorage(cityInput){
+
+      if (cityList.indexOf(cityInput) === -1) {
+            cityList.push(cityInput);
+            window.localStorage.setItem("cityList", JSON.stringify(cityList));
+      
+            createHistoryLi(cityInput);
+          }
+
+
+}
+
+function createHistoryLi(cityInput){
+      var li = $("<li>").addClass("list-group-item list-group-item-action").text(cityInput);
+      $("#history").append(li);
+}
+
+
+function getCurrentWeather(cityInput){
+   //URL to API
+   var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityInput + "&appid=" + APIKey1 + "&units=imperial";
+  
+   // Creating an AJAX call for the specific movie button being clicked
+   $.ajax({
+     url: queryURL,
+     method: "GET"
+   }).then(function(data) {
+ 
+         // create html content for current weather
+         var title = $("<h3>").addClass("card-title").text(data.name + " (" + new Date().toLocaleDateString() + ")");
+
+         var wind = $("<p>").addClass("card-text").text("Wind Speed: " + data.wind.speed + " MPH");
+         var humid = $("<p>").addClass("card-text").text("Humidity: " + data.main.humidity + "%");
+         var temp = $("<p>").addClass("card-text").text("Temperature: " + data.main.temp + " °F");
+
+         var img = $("<img>").attr("src", "http://openweathermap.org/img/w/" + data.weather[0].icon + ".png");
+    
+         title.append(img);
+        
+         getUVIndex(data.coord.lat, data.coord.lon, title, temp, humid, wind);
+         getForecast(cityInput)
+ 
+   });
+}
+
+
+
+function getUVIndex(lat, lon, title, temp, humid, wind){
+      //URL to API
+      var queryURL = `http://api.openweathermap.org/data/2.5/uvi?appid=${APIKey1}&lat=${lat}&lon=${lon}`;
+     
+      // Creating an AJAX call for the specific movie button being clicked
+      $.ajax({
+        url: queryURL,
+        method: "GET"
+      }).then(function(data) {
+            console.log(data)
+
+            var uv = $("<p>").text("UV Index: ");
+            var btn = $("<span>").addClass("btn btn-sm").text(data.value);
+            
+            if (data.value < 3) {
+              btn.addClass("btn-success");
+            }
+            else if (data.value < 7) {
+              btn.addClass("btn-warning");
+            }
+            else {
+              btn.addClass("btn-danger");
+            }
+
+            uv.append(btn)
+
+            $('#currentWeather').empty().append(title, temp, humid, wind, uv);
+
+    
+      });
+   }
+
+   
+function getForecast(cityInput){
+      //URL to API
+      var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityInput + "&appid=" + APIKey1 + "&units=imperial";
+     
+      // Creating an AJAX call for the specific movie button being clicked
+      $.ajax({
+        url: queryURL,
+        method: "GET"
+      }).then(function(data) {
+            console.log(data)
+            $("#forecast").html("<h4 class=\"mt-3\">5-Day Forecast:</h4>").append("<div class=\"row\">");
+
+            for(var i = 0; i < data.list.length; i++){
+                  if(data.list[i].dt_txt.indexOf("15:00:00") !== -1){
+                   var col = $("<div>").addClass("col-md-2");
+                   var card = $("<div>").addClass("card bg-primary text-white");
+                   var body = $("<div>").addClass("card-body");
+                   var title = $("<h5>").addClass("card-title").text(new Date(data.list[i].dt_txt).toLocaleDateString());
+                   var img = $("<img>").attr("src", "http://openweathermap.org/img/w/" + data.list[i].weather[0].icon + ".png");
+                   var p1 = $("<p>").addClass("card-text").text("Temp: " + data.list[i].main.temp_max + " °F");
+                   var p2 = $("<p>").addClass("card-text").text("Humidity: " + data.list[i].main.humidity + "%");
+                   col.append(card.append(body.append(title, img, p1, p2)));
+                   $("#forecast .row").append(col);
+                  }
+            }
+    
+      });
+   }
+
+
+      // Weather
+      //   $("<.city>").text("City: " + response.name);
+      //   $("<.date>").text("Date: " + date);
+      //   $("<.temp>").text("Temperature: " + response.main.temp);
+      //   $("<.humidity>").text("Humidity: " + response.main.humidity);
+      //   $("<.windSpeed>").text("Wind Speed: " + response.wind.speed);
+      //   $("<.icon>").text("Icon: " + response.weather.icon);
+
+      // Forecast
+      //   $("<.city>").text("City: " + response.city.name);
+      //   $("<.date>").text("Date: " + response.list[4].dt_txt);
+      //   $("<.humidity1>").text("Humidity1: " + response.list[4].main.humidity);
+      //   $("<.humidity2>").text("Humidity2: " + response.list[12].main.humidity);
+      //   $("<.humidity3>").text("Humidity3: " + response.list[20].main.humidity);
+      //   $("<.humidity4>").text("Humidity4: " + response.list[28].main.humidity);
+      //   $("<.humidity5>").text("Humidity5: " + response.list[36].main.humidity);
+      //   $("<.temp>").text("Temperature: " + response.list[4].main.temp);
+      //   $("<.icon>").text("Icon: " + response.list[4].weather[0].icon);
